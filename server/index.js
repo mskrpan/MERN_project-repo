e88c7d24f8d98acc545422a7fb10720da85731df
Stjepan Krpan <mskrpan@gmail.com>
2021-03-28 20:48:44 +0200

    osnova dodana i instalirani packagi

---
+++ server/index.js
@@ -0,0 +1,4 @@
+import express from "express";  //dodan express na novi način, prije je bilo "require"
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
